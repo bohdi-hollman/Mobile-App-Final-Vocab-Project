a7b0c2a88a5ea4a6760b5da7a579afafc2fd8732
Bohdi Hollman <bohdi.hollman@gmail.com>
2022-12-12 00:14:43 -0700

    Add Data to Database

--- app/src/main/java/com/example/vocabulary20/fragments/addFragments/addFragment.kt
+++ app/src/main/java/com/example/vocabulary20/fragments/addFragments/addFragment.kt
@@ -1,13 +1,25 @@
 package com.example.vocabulary20.fragments.addFragments
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.vocabulary20.R
+import com.example.vocabulary20.data.Vocab
+import com.example.vocabulary20.viewModel.VocabViewModel
 
 class addFragment : Fragment() {
+
+    lateinit var vocabViewModel: VocabViewModel
+    lateinit var addButton: Button
+    lateinit var vocabText: EditText
+    lateinit var definitionText: EditText
 
 
     override fun onCreateView(
@@ -15,6 +27,39 @@ class addFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        vocabViewModel = ViewModelProvider(this).get(VocabViewModel::class.java)
+
+        vocabText = view.findViewById(R.id.vocabword)
+        definitionText = view.findViewById(R.id.definition)
+        addButton = view.findViewById(R.id.add_vocab)
+
+        addButton.setOnClickListener{
+
+            addVocabToDataBase()
+        }
+
+        return view
     }
+
+    private fun addVocabToDataBase() {
+        val word = vocabText.text.toString()
+        val meaning = definitionText.text.toString()
+        if (inputCheck(word, meaning)) {
+
+            val vocab = Vocab(0, word, meaning)
+
+            vocabViewModel.addVocab(vocab)
+
+            Toast.makeText(requireContext(), "Adding", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(requireContext(), "Invalid Input", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun inputCheck(word: String, meaning: String):Boolean{
+        return !(TextUtils.isEmpty(word) && TextUtils.isEmpty(meaning))
+    }
+
 }
